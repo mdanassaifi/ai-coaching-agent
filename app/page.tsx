6efcd6b222c670aa5929fe3b66b64276d28d5a23
mdanassaifi <cscanas533@gmail.com>
2025-03-22 00:18:56 +0530

    authfixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from '@stackframe/stack';
 import Image from "next/image";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
    <div>
     <h2>Welcome to AI Coaching Agent</h2>
     <Button>Start</Button>
+    <UserButton/>
    </div>
   );
 }

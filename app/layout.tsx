a814d0e4f52b1ba0f2416727319b875003ae674e
mdanassaifi <cscanas533@gmail.com>
2025-03-22 20:59:51 +0530

    scmaset

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { StackProvider, StackTheme } from "@stackframe/stack";
 import { stackServerApp } from "../stack";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Provider from './provider';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,11 +31,14 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       ><StackProvider app={stackServerApp}><StackTheme>
       
-       <provider>
+        <Provider>
+           
+        {children} 
+        
+           </Provider>
+         
 
-          {children}
-      
-       </provider>
+
        
       </StackTheme></StackProvider></body>
     </html>

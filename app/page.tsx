64cc04190be8a263ed046b60644bc792ccc6ac13
MOHD ANAS <cscanas533@gmail.com>
2025-03-29 00:43:35 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
    <div>
-    <h2 >Welcome to AI voice Coaching Agent </h2>
+    <h2 >Welcome to Ai voice Coaching Agent </h2>
     <Button>Start</Button>
     <UserButton/>
    </div>

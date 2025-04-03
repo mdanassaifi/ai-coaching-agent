55a891a9af62de3a72eedd8ca955bef3cd70b648
mdanassaifi <cscanas533@gmail.com>
2025-04-04 00:16:52 +0530

    badd

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,11 @@ export default function Home() {
    <div>
     <h2 className=" font-bold text-red-600 ">Welcome Ai voice Coaching Agent </h2>
     <h1 className=" text-blue-700 font-bold">Stackframe</h1>
+      
 
-    
+       <Button> Click Here </Button>
+    <br />
+     <br />
     <Button>Started</Button>
     <UserButton/>
    </div>

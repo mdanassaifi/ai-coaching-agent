85d19e3eccaa3c34584e62350f6d682f5b14154f
mdanassaifi <cscanas533@gmail.com>
2025-04-04 00:19:49 +0530

    dlt

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,7 @@ export default function Home() {
   return (
    <div>
     <h2 className=" font-bold text-red-600 ">Welcome Ai voice Coaching Agent </h2>
-    <h1 className=" text-blue-700 font-bold">Stackframe</h1>
-      
+  
     <Button>Started</Button>
     <UserButton/>
    </div>

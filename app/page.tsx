3481030576c69aafc4598be2fb7c6ae6330850b3
mdanassaifi <cscanas533@gmail.com>
2025-04-04 00:07:24 +0530

    add2

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,7 @@ export default function Home() {
    <div>
     <h2 >Welcome Ai voice Coaching Agent </h2>
     <h1>Stackframe</h1>
-    <p>Stackframe is a platform that provides AI-powered voice coaching services. Our mission is to help individuals improve their communication skills and confidence through personalized coaching sessions.</p>
-    <p>Our AI voice coaching agent is designed to provide real-time feedback and guidance on your speaking skills. Whether you're preparing for a presentation, interview, or simply want to improve your public speaking abilities, our agent is here to help.</p>
-    <p>With Stackframe, you can practice your speaking skills anytime, anywhere. Our platform is accessible on any device, making it easy to fit coaching sessions into your busy schedule.</p>
+    
     <Button>Started</Button>
     <UserButton/>
    </div>

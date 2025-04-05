8151b93c5d2fadedc909fb672f1f53ce6504b34e
mdanassaifi <cscanas533@gmail.com>
2025-04-06 01:50:20 +0530

    edit

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 function dashboard() {
   return (
-    <div>dashboard</div>
+    <div>Dashboard</div>
   )
 }
 
--- middleware.jsx
+++ middleware.jsx
@@ -1,8 +1,9 @@
 import { StackServerApp } from "@stackframe/stack";
 import { NextResponse } from "next/server";
 
+
 export async function middleware(request) {
-    const user = await StackServerApp.getUser();
+    const user = await StackServerApp.getUser(request);
     if (!user) {
       return NextResponse.redirect(new URL('/handler/sign-in', request.url));
     }

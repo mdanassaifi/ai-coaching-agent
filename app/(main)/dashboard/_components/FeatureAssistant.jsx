c23dbe58e3dee6e3ec4a620d2f45ce157aa65300
mdanassaifi <cscanas533@gmail.com>
2025-04-06 15:13:13 +0530

    pagecr

--- app/(main)/dashboard/_components/FeatureAssistant.jsx
+++ app/(main)/dashboard/_components/FeatureAssistant.jsx
@@ -1,8 +1,20 @@
+"use client"
+import { Button } from '@/components/ui/button';
+import { useUser } from '@stackframe/stack'
 import React from 'react'
 
 function FeatureAssistant() {
+  const user = useUser();
   return (
-    <div>FeatureAssistant</div>
+    <div>
+        <div className='flex justify-between items-center'>
+           <div>
+             <h2 className='font-medium text-gray-500'>My Workspace </h2>
+             <h2 className='text-3xl font-bold'>Welcome Back, {user?.displayName} </h2>
+      </div>
+      <Button>Profile</Button>
+     </div>
+    </div>
   )
 }
 

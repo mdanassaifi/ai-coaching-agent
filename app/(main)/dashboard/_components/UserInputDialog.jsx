4129b6e6b26a986ef2beec6ee767d10de8377049
mdanassaifi <cscanas533@gmail.com>
2025-04-14 01:14:49 +0530

    set

--- app/(main)/dashboard/_components/UserInputDialog.jsx
+++ app/(main)/dashboard/_components/UserInputDialog.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Dialog,
     DialogContent,
@@ -13,6 +13,10 @@ import Image from 'next/image'
 
   
 function UserInputDialog({children, ExpertsList}) {
+  
+    const [selectedExpert,setSelectedExpert]=useState();
+
+
   return (
     <Dialog>
     <DialogTrigger>{children}</DialogTrigger>
@@ -27,13 +31,17 @@ function UserInputDialog({children, ExpertsList}) {
                <h2 className='text-black mt-5'> Enter a topic to master your skills in {ExpertsList.name} </h2>
                 <div className='grid grid-cols-3 md:grid-cols-5 gap-6 mt-3'>
                   {CoachingExpert.map((expert,index)=>(
-                    <div key={index} >
+                    <div key={index} onClick={()=> setSelectedExpert (expert.name)} 
+                    className={`${selectedExpert == expert.name && 'border-2'} p-1 rounded-2xl `}
+                    >
                     <Image src={expert.avatar} alt={expert.name}
                         width={100}
                         height={100}
-                        className='rounded-2xl h-[80px] w-[80px] object-cover'
+                        className='rounded-2xl h-[80px] w-[80px] object-cover
+                        hover:scale-105 transition-all cursor-pointer
+                        '
                     />
-                    <h2>{expert.name}</h2>
+                    <h2 className='text-center'>{expert.name}</h2>
                     </div>
                   ))}
 

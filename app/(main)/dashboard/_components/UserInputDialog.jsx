1ae1b8d1b115340343151475311cf0d5a29bfc11
mdanassaifi <cscanas533@gmail.com>
2025-04-14 01:33:12 +0530

    badd

--- app/(main)/dashboard/_components/UserInputDialog.jsx
+++ app/(main)/dashboard/_components/UserInputDialog.jsx
@@ -10,6 +10,8 @@ import {
 import { Textarea } from '@/components/ui/textarea'
 import { CoachingExpert } from '@/services/options'
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { Ghost } from 'lucide-react'
 
   
 function UserInputDialog({children, ExpertsList}) {
@@ -31,20 +33,25 @@ function UserInputDialog({children, ExpertsList}) {
                <h2 className='text-black mt-5'> Enter a topic to master your skills in {ExpertsList.name} </h2>
                 <div className='grid grid-cols-3 md:grid-cols-5 gap-6 mt-3'>
                   {CoachingExpert.map((expert,index)=>(
-                    <div key={index} onClick={()=> setSelectedExpert (expert.name)} 
-                    className={`${selectedExpert == expert.name && 'border-2'} p-1 rounded-2xl `}
-                    >
+                    <div key={index} onClick={()=> setSelectedExpert (expert.name)} >
+                    
                     <Image src={expert.avatar} alt={expert.name}
                         width={100}
                         height={100}
-                        className='rounded-2xl h-[80px] w-[80px] object-cover
-                        hover:scale-105 transition-all cursor-pointer
-                        '
-                    />
+                        className={`rounded-2xl h-[80px] w-[80px] object-cover
+                        hover:scale-105 transition-all cursor-pointer p-1 border-primary
+                        ${selectedExpert == expert.name && 'border-2'}
+                         `}
+                      />
+                   
                     <h2 className='text-center'>{expert.name}</h2>
                     </div>
                   ))}
 
+                </div>
+                <div className='flex gap-5 justify-end'>
+                  <Button variant={'ghost'}>Cancel</Button>
+                  <Button> Next </Button>
                 </div>
             </div>
         </DialogDescription>

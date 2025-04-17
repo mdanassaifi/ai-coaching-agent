72dbfad11ad079cccf6e88ae808bb0b670ce5923
mdanassaifi <cscanas533@gmail.com>
2025-04-18 03:08:05 +0530

    savedata

--- app/(main)/discussion-room/[roomid]/page.jsx
+++ app/(main)/discussion-room/[roomid]/page.jsx
@@ -1,13 +1,24 @@
 "use client"
 import { api } from '@/convex/_generated/api';
+import { CoachingExpert } from '@/services/options';
 import { useQuery } from 'convex/react';
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function DiscussionRoom() {
     const {roomid}= useParams();
     const DiscussionRoomData=useQuery(api.DiscussionRoom.GetDiscussionRoom,{id:roomid});
-    console.log(DiscussionRoomData);
+    const [expert, setExpert] = useState();
+
+      useEffect(()=>{ 
+        if (DiscussionRoomData)
+             { 
+            const Expert = CoachingExpert.find(item => item.name == DiscussionRoomData.expertName);
+            console .log(Expert);
+            setExpert(Expert);
+          }
+        },[DiscussionRoomData])
+
 
   return (
     <div>DiscussionRoom</div>
